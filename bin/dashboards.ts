cd3f24beda18b505fd00186780805a13593bf4ed
mannau <mario.annau@quantargo.com>
2022-03-10 10:25:51 +0100

    Create Initial Shiny Dashboard

--- bin/dashboards.ts
+++ bin/dashboards.ts
@@ -15,7 +15,8 @@ new DashboardsStack(app, 'DashboardsStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  // env: { account: '1234567890', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
+
